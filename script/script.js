e830955c93f5f258b029263e08f09051db87f190
AllaGutsul27 <alla-solomiychuk@i.ua>
2022-08-13 18:20:03 +0300

    Added current position weather

--- script/script.js
+++ script/script.js
@@ -37,9 +37,8 @@ function showTemperature(response) {
     description.innerHTML = response.data.weather[0].description;
     let iconElement = document.querySelector("#icon");
     iconElement.setAttribute("src", `/images/${response.data.weather[0].icon}.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-
-
 
 function search(event) {
     event.preventDefault();
@@ -59,48 +58,41 @@ formSearch.addEventListener("submit", search);
 
 
 // //----- Температура за поточним місцем знаходження (кнопка Current) ------//
-// function showTemperatureCurrent(response) {
-//     let degreesValueCurrent = document.querySelector(".search-degrees");
-//     degreesValueCurrent.innerHTML = Math.round(response.data.main.temp);
-//     let searchCity = document.querySelector(".search-city");
-//     searchCity.innerHTML = response.data.name;
+function showTemperatureCurrent(response) {
+    console.log(response)
+    let degrees = document.querySelector(".search-degrees");
+    degrees.innerHTML = Math.round(response.data.main.temp);
+    let searchCity = document.querySelector(".search-city");
+    searchCity.innerHTML = response.data.name;
 
-//     let wind = document.querySelector(".wind-value");
-//     wind.innerHTML = Math.round(response.data.wind.speed);
-//     let humidity = document.querySelector(".humidity-value");
-//     humidity.innerHTML = response.data.main.humidity;
+    let wind = document.querySelector(".wind-value");
+    wind.innerHTML = Math.round(response.data.wind.speed);
+    let humidity = document.querySelector(".humidity-value");
+    humidity.innerHTML = response.data.main.humidity;
 
+    let description = document.querySelector("#description");
+    description.innerHTML = response.data.weather[0].description;
+    let iconElement = document.querySelector("#icon");
+    iconElement.setAttribute("src", `/images/${response.data.weather[0].icon}.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
+}
 
+function showPosition(position) {
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+    let apiKey = "28b89a027ca36429077890b9084e664e";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric`;
+    axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperatureCurrent);
+}
 
-// }
+function getCurrentLocation(event) {
+    event.preventDefault;
+    navigator.geolocation.getCurrentPosition(showPosition);
+}
 
-//     function showPosition(position) {
-//     let lat = position.coords.latitude;
-//     let lon = position.coords.longitude;
-//     let apiKey = "28b89a027ca36429077890b9084e664e";
-//     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-//     axios.get(apiUrl).then(showTemperatureCurrent);
-// }
-
-
-// function getCurrentLocation(event) {
-//     event.preventDefault;
-//     navigator.geolocation.getCurrentPosition(showPosition);
-// }
-
-// let current = document.querySelector("#current");
-// current.addEventListener("click", getCurrentLocation);
+let current = document.querySelector("#current");
+current.addEventListener("click", getCurrentLocation);
     
-
-
-
-
-
-
-
-
-    // let iconElement = document.querySelector("#icon");
-    // iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 
 
 
